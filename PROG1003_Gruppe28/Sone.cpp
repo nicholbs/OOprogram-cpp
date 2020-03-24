44af46b09528ecac9e37f0408630cd14e30ae852
Oddbjorn <oddbjorn.sb@stud.ntnu.no>
2020-03-24 10:36:07 +0100

    Ny sone; nytt oppdrag

--- PROG1003_Gruppe28/Sone.cpp
+++ PROG1003_Gruppe28/Sone.cpp
@@ -1,1 +1,25 @@
 #include "Sone.h"
+#include "Bolig.h"
+#include "Enebolig.h"
+#include <iostream>
+#include <ctype.h>
+
+using namespace std;
+
+Sone::Sone(int id) {
+	ID = id;
+}
+
+void Sone::nyBolig(int onr)
+{
+	cout << "Leilighet/enebolig [L/E]: ";
+	char valg; cin >> valg;
+
+	Bolig* p = nullptr;
+	if (toupper(valg) == 'L')
+		p = new Bolig(onr);
+	else if (toupper(valg) == 'E')
+		p = new Enebolig(onr);
+
+	boligerTilSalgs.push_back(p);
+}
--- PROG1003_Gruppe28/Soner.cpp
+++ PROG1003_Gruppe28/Soner.cpp
@@ -1,1 +1,18 @@
 #include "Soner.h"
+#include "Sone.h"
+#include <iostream>
+
+void Soner::nySone(int snr)
+{
+	Sone* sp = new Sone(snr);
+	soneMap.insert(make_pair(snr, sp));
+	cout << "Opprettet ny Sone " << snr << "!\n\n";
+}
+
+void Soner::nyBolig(int snr)
+{
+	soneMap[snr]->nyBolig(sisteNr + 1);
+	//Finnes Bolig (sisteNr + 1)?
+	sisteNr++;
+	cout << "Opprettet ny Bolig " << sisteNr << "!\n\n";
+}
