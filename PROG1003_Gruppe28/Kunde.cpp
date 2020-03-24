22b4681f013b558bed8ac3358a953caa2adfa0a6
oddhb <oddhb@stud.ntnu.no>
2020-03-24 14:35:51 +0100

    Endringer i kunde.*

--- PROG1003_Gruppe28/Kunde.cpp
+++ PROG1003_Gruppe28/Kunde.cpp
@@ -6,6 +6,7 @@
 #include <string>
 using namespace std;
 
+extern Soner gSoner; //Legger til Gsonr for å kunne utføre sjekk og mappinger
 /**
  * Dette er parameterfylt konstruktor, far medsendt id og ber bruker lese inn
  *
@@ -28,16 +29,17 @@ Kunde::Kunde(int nr) {
     //Sikrer innlesning av tlfnr
     telefon = lesInt("\nTelefonnummer:",0,99999999);
     //IMPLEMENTERES ENUM OG VECTORINNLESNING
+    //Registrerer soner
     cout <<"\nRegistrer sone J trykk Q for avslutt";
     kommando = lesChar("");
     while(kommando !='Q') {
-        soneInnlest = lesInt("\nSonenr:",1,10);
-            /**
-            if(Soner.finnesSone(soneInnlest)){
-                kundeSone.push_back(soneInnlest);
-            }
-            else cout <<"\nFinner ikke sone med nr: " <<soneInnlest;
-        **/
+       soneInnlest = lesInt("\nSonenr:",1,MAX_SONER);
+       //Sa lenge sonen finnes registrer den
+       if(gSoner.finnesSone(soneInnlest)){
+        kundeSoner.push_back(soneInnlest);
+       }
+       else cout <<"\nFinner ikke sonenr: " <<soneInnlest;
+
         kommando = lesChar("\nRegistrer sone J trykk Q for avslutt");
 
     }
--- PROG1003_Gruppe28/Kunde.h
+++ PROG1003_Gruppe28/Kunde.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include "boligtype.h"
+#include "Soner.h"
 
 
 using namespace std;
